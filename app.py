a42e0680c636ce2c722bef34f2afc7116c4443b5
Ben Edwards <ben@edwards.nz>
2022-08-18 15:52:52 +1200

    Tweak YML

--- app.py
+++ app.py
@@ -12,46 +12,53 @@ class AccountAPI(MethodView):
         ---
         tags:
           - accounts
+        summary: Return a list of accounts
+        description: Return a list of accounts
         definitions:
           - schema:
-            id: Group
+            id: Account
             properties:
-            name:
-              type: string
-              description: the group's name
+              name:
+                type: string
+                description: The Account Name
+              phone:
+                type: string
+                description: The Account Phone Number
+              website:
+                type: string
+                description: The Account Website
+              
         responses:
           200:
             description: Successfully returned accounts
+            content:
+              application/json:
+                schema:
+                  type: array
+                  items:
+                    - schema:
+                        id: Account
+                        properties:
+                        name:
+                            type: string
+                            description: The Account Name
+                        phone:
+                            type: string
+                            description: The Account Phone Number
+                        website:
+                            type: string
+                            description: The Account Website
         """
         return [
             {
                 'name': 'Ben Enterprises',
                 'phone': '021 111 222 333',
-                'website': 'https://www.google.com',
-                'contacts': [
-                    {
-                        'firstName': 'Ben',
-                        'lastName': 'Edwards',
-                        'email': 'ben@edwards.nz'
-                    },
-                    {
-                        'firstName': 'Bob',
-                        'lastName': 'Jones',
-                        'email': 'bob@jones.com'
-                    },
-                ]
+                'website': 'https://www.google.com'
             },
             {
                 'name': 'Evil Conglomerate',
                 'phone': '09 123 456',
-                'website': 'https://www.google.com',
-                'contacts': [
-                    {
-                        'firstName': 'Evil',
-                        'lastName': 'Kenevil',
-                        'email': 'evil@kenevil.com'
-                    },
-                ]
+                'website': 'https://www.google.com'
             }
         ]
 
