bca37606ce21431b77f5c94c9818ae72488ce536
Ben Edwards <ben@edwards.nz>
2022-08-18 15:42:13 +1200

    Add spec

--- app.py
+++ app.py
@@ -1,37 +1,70 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask.views import MethodView
+from flask_swagger import swagger
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return [
-        {
-            'name': 'Ben Enterprises',
-            'phone': '021 111 222 333',
-            'website': 'https://www.google.com',
-            'contacts': [
-                {
-                    'firstName': 'Ben',
-                    'lastName': 'Edwards',
-                    'email': 'ben@edwards.nz'
-                },
-                {
-                    'firstName': 'Bob',
-                    'lastName': 'Jones',
-                    'email': 'bob@jones.com'
-                },
-            ]
-        },
-        {
-            'name': 'Evil Conglomerate',
-            'phone': '09 123 456',
-            'website': 'https://www.google.com',
-            'contacts': [
-                {
-                    'firstName': 'Evil',
-                    'lastName': 'Kenevil',
-                    'email': 'evil@kenevil.com'
-                },
-            ]
-        }
-    ]
+class AccountAPI(MethodView):
+
+    def get(self):
+        """
+        Return a dummy endpoint
+        ---
+        tags:
+          - accounts
+        definitions:
+          - schema:
+            id: Group
+            properties:
+            name:
+              type: string
+              description: the group's name
+        responses:
+          200:
+            description: Successfully returned accounts
+        """
+        return [
+            {
+                'name': 'Ben Enterprises',
+                'phone': '021 111 222 333',
+                'website': 'https://www.google.com',
+                'contacts': [
+                    {
+                        'firstName': 'Ben',
+                        'lastName': 'Edwards',
+                        'email': 'ben@edwards.nz'
+                    },
+                    {
+                        'firstName': 'Bob',
+                        'lastName': 'Jones',
+                        'email': 'bob@jones.com'
+                    },
+                ]
+            },
+            {
+                'name': 'Evil Conglomerate',
+                'phone': '09 123 456',
+                'website': 'https://www.google.com',
+                'contacts': [
+                    {
+                        'firstName': 'Evil',
+                        'lastName': 'Kenevil',
+                        'email': 'evil@kenevil.com'
+                    },
+                ]
+            }
+        ]
+
+app.add_url_rule("/", view_func=AccountAPI.as_view("accounts"))
+
+
+@app.route("/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "Ben\'s Cool API"
+    swag['info']['description'] = "Such a cool API"
+    #swag['info']['contact']['email'] = "ben@edwards.nz"
+    return jsonify(swag)
+
+
