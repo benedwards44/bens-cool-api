b3a8b26d3720cae3f6bb133a5b4a21538b12f48a
Ben Edwards <ben@edwards.nz>
2022-08-18 15:10:22 +1200

    Return JSON array

--- app.py
+++ app.py
@@ -4,4 +4,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return [
+        {
+            'name': 'Ben Enterprises',
+            'phone': '021 111 222 333',
+            'website': 'https://www.google.com',
+            'contacts': [
+                {
+                    'firstName': 'Ben',
+                    'lastName': 'Edwards',
+                    'email': 'ben@edwards.nz'
+                },
+                {
+                    'firstName': 'Bob',
+                    'lastName': 'Jones',
+                    'email': 'bob@jones.com'
+                },
+            ]
+        },
+        {
+            'name': 'Evil Conglomerate',
+            'phone': '09 123 456',
+            'website': 'https://www.google.com',
+            'contacts': [
+                {
+                    'firstName': 'Evil',
+                    'lastName': 'Kenevil',
+                    'email': 'evil@kenevil.com'
+                },
+            ]
+        }
+    ]
